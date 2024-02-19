292f9d3b56727152e30075b759922ac2022c2966
GauravFrontend <gaurav9911093928@gmail.com>
2024-02-19 20:42:26 +0530

    bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -91,13 +91,13 @@ function Header() {
                 >
                     <div className="px-8">
                         <div className="flex flex-col gap-8 font-bold tracking-wider">
-                            <Link to="/" onClick={() => toggleMenu = false} className="border-l-4 border-gray-600">
+                            <Link to="/" onClick={() => setToggleMenu = false} className="border-l-4 border-gray-600">
                                 Home
                             </Link>
-                            <Link to="/login" onClick={() => toggleMenu = false} className="border-l-4 border-gray-600">
+                            <Link to="/login" onClick={() => setToggleMenu = false} className="border-l-4 border-gray-600">
                                 Login
                             </Link>
-                            <Link to="/signup" onClick={() => toggleMenu = false} className="border-l-4 border-gray-600">
+                            <Link to="/signup" className="border-l-4 border-gray-600">
                                 Signup
                             </Link>
                         </div>

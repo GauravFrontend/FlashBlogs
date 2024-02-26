228db6c8525a2b0a749a4f5f1d33f1ed23c00bf6
GauravFrontend <gaurav9911093928@gmail.com>
2024-02-26 06:25:59 +0530

    added a cool loader animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import service from '../appwrite/config';
 import PostCard from '../components/PostCard';
+import BoltLoader from '../components/BoltLoader';
 
 
 const Home = () => {
@@ -19,28 +20,34 @@ const Home = () => {
     <>
       {posts ? (
         <div >
-				<div className=" flex flex-wrap justify-start align-middle gap-5" >
+          <div className=" flex flex-wrap justify-start align-middle gap-5" >
 
-					{posts.map((post) => {
-						return <div className='flex' key={post.$id} >
-							<PostCard {...post} />
-						</div>
-					})}
-				</div>
-			</div>
+            {posts.map((post) => {
+              return <div className='flex' key={post.$id} >
+                <PostCard {...post} />
+              </div>
+            })}
+          </div>
+        </div>
       ) : (
-        <div className='h-96 flex flex-col justify-center align-middle' >
-          <h1 className='text-2xl'>loading</h1>
+        <div className='h-96 p-12 flex justify-center align-middle' >
+          <BoltLoader
+            className={"loaderbolt "}
+            boltColor={"#5383FF"}
+            desktopSize="50px"
+            mobileSize="30px"
+            backgroundBlurColor={"#FFBB12"}
+          />
         </div>
       )}
     </>
   ) : (
     <>
-      
+
       <div className='h-96 flex flex-col justify-center align-middle' >
-      <h1 className='text-3xl' >Welcome to Home</h1>
-      <h1 className='text-3xl' >Please Login</h1>
-        </div>
+        <h1 className='text-3xl' >Welcome to Home</h1>
+        <h1 className='text-3xl' >Please Login</h1>
+      </div>
     </>
   )
 }
